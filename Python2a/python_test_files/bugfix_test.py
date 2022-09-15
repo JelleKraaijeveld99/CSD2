e9293a7b808cf1e51d9ff037f4a33733ab099c27
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2022-09-15 15:27:59 +0200

    initial commit : added bugfix_test.py

---
+++ Python2a/python_test_files/bugfix_test.py
@@ -0,0 +1,16 @@
+#a while loop, loops a set of statements as long as the condition is true 
+
+while True:
+    try:
+        getal = int(input("voer een getal in "))
+    except ValueError:
+        print("Sorry, can u please give a number")
+    
+    else: 
+        #break stops the loop   
+        break 
+
+print(getal)
+
+
+#try a while loop in a for loop the next time i code
