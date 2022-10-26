abba59efef0fbc00aed8e5e75bb8b9a5156f26c2
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2022-10-26 15:46:38 +0200

    new version: irregular_beat_generator.py

--- Python2a/eindopdracht/irregular_beat_generator.py
+++ Python2a/eindopdracht/irregular_beat_generator.py
@@ -1,3 +1,4 @@
+from os import times
 import simpleaudio as sa 
 import time as time 
 import random 
@@ -14,7 +15,6 @@ instrument_list = kick, clap, tambourine
 default_bpm = 60
 bars_amount = 4
 time_signature = 0
-
 
 #communication with user
 print("\nHi, welcome to the irregular beat generator. This program will generate an irregular beat according to a given BPM, amount of bars and either a 5/4 or a 7/8 time signature. Lets start!\n")
@@ -47,14 +47,12 @@ while retrieve_time_signature:
     except:
         print("\nSorry, can u please give the number 1 or 2")
     else: 
-        if time_signature is 1 or time_signature is 2:
+        if time_signature == 1 or time_signature == 2:
             retrieve_time_signature = False
         else: 
          print("\nSorry, can u please give the number 1 or 2")
 
-
-
-########## section for generating rhythms   
+########## section for generating rhythms ###########  
 
 #function for amount of eigtht notes according to the time signature 
 def time_sign_to_notes(signature):
@@ -71,7 +69,7 @@ def time_sign_to_notes(signature):
 amount_of_notes = time_sign_to_notes(time_signature)
 
 #function for generating euclidean rhythms 
-def euclidean_gen(num_pulses, num_notes):
+def euclidean_gen(num_pulses, num_notes, offset):
     # calculate duration of a note, expressed in 16th
     note_dur = int(num_pulses / num_notes)
     # fill list num_notes times with the duration value
@@ -81,22 +79,46 @@ def euclidean_gen(num_pulses, num_notes):
     for i in range(rest_value):
         sequence[i] += 1    
 
-    #summing up all the notes and converting them to timestamps
+    #summing up all the notes and converting them to durations
     timestamp_sequence = []
     sum = 0
     timestamp_sequence.append(0)
     for x in range(len(sequence)-1):
         sum = int(sum) + int(sequence[x])
         timestamp_sequence.append(sum)
+    print("hoi dit is de sequence voor de offset:", timestamp_sequence)
+    
+    #section for offset 
+    timestamp_sequence = [x+offset for x in timestamp_sequence] #add the offset value to every dur in the list   
+    
+    #modulo all the new values in the list based on the maximum amount of possible notes
+    for i in range(len(timestamp_sequence)):
+        timestamp_sequence[i] = timestamp_sequence[i] % num_pulses
+    
+    #sort all the values in the right order from low to high
+    timestamp_sequence.sort()
     return timestamp_sequence
 
-kick_pattern = euclidean_gen(amount_of_notes,random.randint(1,4))
-clap_pattern = euclidean_gen(amount_of_notes,random.randint(1,4))
-tambourine_pattern = euclidean_gen(amount_of_notes,random.randint(1,amount_of_notes - 3))
+kick_pattern = euclidean_gen(amount_of_notes,random.randint(1,4), 0)
+clap_pattern = euclidean_gen(amount_of_notes,random.randint(1,4), 0)
+tambourine_pattern = euclidean_gen(amount_of_notes,random.randint(3,amount_of_notes - 3), 0)
 
+#function for making variations in the existing eucl rhythm
+def rhythm_bars(eucl_list, bars, note_amount):
+    buffer_list = eucl_list # changing this list every step in the for loop, adding the amount of notes * the amount of bars to every item in the list
+    complete_eucl = []
+    for i in range(bars):
+        buffer_list = [x + i*note_amount for x in eucl_list]
+        print(i, "=", buffer_list)
+        complete_eucl.extend(buffer_list)
+    return complete_eucl
+  
 print("this is the kick pattern:",kick_pattern)
 print("this is the clap pattern:",clap_pattern)
 print("this is the tambourine pattern:",tambourine_pattern)
+
+total_rhythm = rhythm_bars(tambourine_pattern, bars_amount, amount_of_notes)
+print("this is the total rhythm:",total_rhythm) 
 
 
 
