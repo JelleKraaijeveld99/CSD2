fa8b84bbd71889e75137ad579ea9c22b479e06d3
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2022-11-09 16:58:25 +0100

    WIP: irregular_beat_generator.py

--- Python2a/eindopdracht/irregular_beat_generator.py
+++ Python2a/eindopdracht/irregular_beat_generator.py
@@ -141,6 +141,46 @@ print("this is the total kick rhythm:", total_rhythm_k)
 print("this is the total clap rhythm:", total_rhythm_c) 
 print("this is the total tambourine rhythm:", total_rhythm_tb) 
 
+#section for converting ts to durations so i can export to midi later on 
+def ts_to_dur_events(ts_list,x_bpm, instr_name):
+    #empty list for durations
+    dur_list = []
+
+    #duration in of an eigtht note in ms:
+    e_note = 30000/x_bpm
+
+    #reverse the ts list to avoid index error in the for loop
+    ts_list.reverse()
+
+    # subtract 2 consecutive values in the ts_list to calculate the difference in value between them. This must be the duration
+    for i in range(len(ts_list)-1):
+        dur_note = ts_list[i] - ts_list[i+1]
+        dur_list.append(dur_note * e_note)
+    
+    #add the last timestamp value (which is the first because i reversed the list) to the duration list. 
+    dur_list.append(ts_list.pop()*e_note)
+    
+    #now reverse the duration list so all the durations are in the right order again. 
+    dur_list.reverse()
+
+    #section for making the events
+    event_list = []
+    for x in range(len(dur_list)):
+           event_list.append({'duration': dur_list[x], 'instrument': instr_name})
+    
+    return event_list
+
+all_dur_tb = ts_to_dur_events(total_rhythm_tb)
+all_dur_k = ts_to_dur_events(total_rhythm_k)
+all_dur_c = ts_to_dur_events(total_rhythm_c)
+
+dur_combined = all_dur_tb + all_dur_c + all_dur_c
+
+
+print("durations tb:", all_dur_tb)
+print("durations k:", all_dur_k)
+print("durations c:", all_dur_c)
+
 #function for convertion timestamps in 8th notes to timestamps in actual time
 def stamps8th_to_stampstime(x_bpm, y_ts_8th):
     time_stamps_time = []
@@ -170,7 +210,7 @@ tambourine_events = event_maker(timestamps_tb, tambourine)
 all_events = kick_events + clap_events + tambourine_events
 
 all_events = sorted(all_events, key=lambda x: x['timestamp'])
-print("after the sort:", all_events)
+# print("after the sort:", all_events)
 
 #section of the code for the audio playback
 start_time = time.time()
