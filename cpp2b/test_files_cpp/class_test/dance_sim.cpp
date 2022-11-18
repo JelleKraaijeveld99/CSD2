b70d76b8ea328681ed8015256d40f936695ced0a
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2022-11-18 14:29:30 +0100

    modified: dance_sim

--- cpp2b/test_files_cpp/class_test/dance_sim.cpp
+++ cpp2b/test_files_cpp/class_test/dance_sim.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <string> 
 #include <list> 
+#include  <cstdlib>
 using namespace std;
 
 class dance
@@ -10,32 +11,47 @@ public:
   string move;
   string dancestyle;
   string musicgenre;
-  
+
+  string ingredients[11] = {"step to the left", "step to the right", "hands up", "hands down", "right hand up", "right hand down", "shuffle", "left hand up", "left hand down", "moonwalk", "floss"};
+
+  int random_val;
 
 void move_selector()
 {
-move = "step to the left";
+random_val = rand() % 11;
+move = ingredients[random_val];
 }
 
 void dancestyle_selector()
 {
-dancestyle = "salsa";
+  dancestyle = "salsa";
 }
 
 void musicgenre_selector()
 {
-musicgenre = "techno";
+  musicgenre = "techno";
 }
 
 void dance_printer()
 {
-cout << "This is the dance move: " << move << endl; 
+  cout << "This is the dance move: " << move << endl; 
+}
+
+void genre_style_printer()
+{
+  cout << "This is the genre: " << musicgenre << endl << "This is the dancestyle: " << dancestyle << endl; 
 }
 };
 
 int main()
 {
 dance my_dance;
-my_dance.move_selector();
-my_dance.dance_printer(); 
+my_dance.dancestyle_selector();
+my_dance.musicgenre_selector();
+my_dance.genre_style_printer();
+for(int i = 0; i < 6; i++)
+  {
+    my_dance.move_selector();
+    my_dance.dance_printer();
+  }
 }
