7bb24283884ef00baa43eb0df533fa88b1df3257
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2022-12-08 12:08:55 +0100

    added strucure: eindopdracht

---
+++ cpp2b/assignments/eindopdracht/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include "jack_module.h"
+#include "math.h"
+#include "writeToFile.h"
+#include "oscillator.h"
+#include "callback.h"
+
+#define WRITE_TO_FILE 1
+
+int main(int argc, char **argv) {
+  auto callback = CustomCallback{};
+  auto jackModule = JackModule{callback};
+
+#if WRITE_TO_FILE
+  AudioToFile audioToFile;
+  audioToFile.write(callback);
+#else
+
+  jackModule.init(0, 1);
+
+  bool running = true;
+  while (running) {
+    switch (std::cin.get()) {
+      case 'q':
+        running = false;
+    }
+  }
+#endif
+  //end the program
+  return 0;
+} // main()
