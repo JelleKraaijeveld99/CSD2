20b9703cd4f4129ebf91fe117144c4cfde6c4c6d
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2023-01-29 15:04:51 +0100

    Bug fixes

--- Python2a/eindopdracht/irregular_beat_generator.py
+++ Python2a/eindopdracht/irregular_beat_generator.py
@@ -16,49 +16,7 @@ default_bpm = 60
 bars_amount = 4
 time_signature = 0
 
-#communication with user
-print("\nHi, welcome to the irregular beat generator. This program will generate an irregular beat according to a given BPM, amount of bars and either a 5/4 or a 7/8 time signature. Lets start!\n")
-
-# input the bpm 
-retrieve_bpm = True
-while retrieve_bpm:
-    try:
-        default_bpm = int(input("\nAt what BPM do you want the sequence to be played? Please give a number between 50 and 180\n"))
-    except:
-        print("\nSorry, can u please give a number between 50 and 180")
-    else: 
-        if default_bpm in range(50,181):
-            retrieve_bpm = False
-        else:
-         print("\nSorry, can u please give a number between 50 and 180")
-
-#input the amount of bars
-retrieve_bars = True
-while retrieve_bars:
-    try:
-        bars_amount = int(input("\nHow many bars do you want to be generated? Please give a number between 0 and 16\n"))
-    except:
-        print("\nSorry, can u please give a number between 0 and 8")
-    else:
-        if bars_amount in range(0,9):
-            retrieve_bars = False
-        else:
-         print("\nSorry, can u please give a number between 0 and 8")
-
-#input the time signature 
-retrieve_time_signature = True
-while retrieve_time_signature:
-    try:
-        time_signature = int(input("\nWhat time signature do you want to use? Press 1 for a 5/4 time signature or press 2 for a 7/8 time signature.\n"))
-    except:
-        print("\nSorry, can u please give the number 1 or 2")
-    else: 
-        if time_signature == 1 or time_signature == 2:
-            retrieve_time_signature = False
-        else: 
-         print("\nSorry, can u please give the number 1 or 2")
-
-########## section for generating rhythms ###########  
+########## section for all the functions i use in my program ###########  
 
 #function for amount of eigtht notes according to the time signature 
 def time_sign_to_notes(signature):
@@ -70,9 +28,6 @@ def time_sign_to_notes(signature):
     if signature == 2: 
         eighth_notes = 7 
         return eighth_notes
-
-#variable for storing the amount of eigtht notes per bar
-amount_of_notes = time_sign_to_notes(time_signature)
 
 #function for generating euclidean rhythms 
 def euclidean_gen(num_pulses, num_notes, offset):
@@ -104,10 +59,6 @@ def euclidean_gen(num_pulses, num_notes, offset):
     timestamp_sequence.sort()
     return timestamp_sequence
 
-kick_pattern = euclidean_gen(amount_of_notes,random.randint(1,5),0) #starting with an offset of 0 so the rhythm always starts with a kick
-clap_pattern = euclidean_gen(amount_of_notes,random.randint(1,5), random.randint(2,6)) #starting with an random offset between 2 and 6 so the clap will always be after the kick in the first bars
-tambourine_pattern = euclidean_gen(amount_of_notes,random.randint(3,amount_of_notes - 3), random.randint(0,5))
-
 #function for making variations (after a customizable amount of bars) for the original generated rhythm
 def rhythm_bars(eucl_list, bars, note_amount):
     buffer_list = eucl_list # a list i use to store a rhythm + the amount of bars we are in right now
@@ -133,9 +84,6 @@ def rhythm_bars(eucl_list, bars, note_amount):
 
     return complete_eucl
 
-total_rhythm_tb = rhythm_bars(tambourine_pattern, bars_amount, amount_of_notes)
-total_rhythm_k = rhythm_bars(kick_pattern, bars_amount, amount_of_notes)
-total_rhythm_c = rhythm_bars(clap_pattern, bars_amount, amount_of_notes)
 
 #section for converting ts to durations so i can export to midi later on 
 def ts_to_dur_list(ts_list,x_bpm):
@@ -162,12 +110,6 @@ def ts_to_dur_list(ts_list,x_bpm):
 
     return dur_list
 
-all_dur_tb = ts_to_dur_list(total_rhythm_tb, default_bpm)
-all_dur_k = ts_to_dur_list(total_rhythm_k, default_bpm)
-all_dur_c = ts_to_dur_list(total_rhythm_c, default_bpm)
-dur_combined = all_dur_tb + all_dur_c + all_dur_c
-
-
 #function for convertion timestamps in 8th notes to timestamps in actual time
 def stamps8th_to_stampstime(x_bpm, y_ts_8th):
     time_stamps_time = []
@@ -177,10 +119,6 @@ def stamps8th_to_stampstime(x_bpm, y_ts_8th):
     for ts in y_ts_8th:
         time_stamps_time.append(e_note*ts)
     return time_stamps_time
-
-timestamps_k = stamps8th_to_stampstime(default_bpm, total_rhythm_k)
-timestamps_c = stamps8th_to_stampstime(default_bpm, total_rhythm_c)
-timestamps_tb = stamps8th_to_stampstime(default_bpm, total_rhythm_tb)
  
 #function for making events according to the timestamps and a random instrument from the instrument list, also give to instruments a midinote value for exporting midi
 def event_maker(t_stamp, sample, durations, instrument, midinote):
@@ -189,102 +127,206 @@ def event_maker(t_stamp, sample, durations, instrument, midinote):
            event_list.append({'timestamp': t_stamp[x], 'sample': sample, 'duration': durations[x], 'instrument': instrument, 'midinote': midinote })
     return event_list
 
-#making a list with events with the funtion event_maker
-kick_events = event_maker(timestamps_k, kick, all_dur_k, 'kick', 36)
-clap_events = event_maker(timestamps_c, clap, all_dur_c, 'clap', 39)
-tambourine_events = event_maker(timestamps_tb, tambourine, all_dur_tb, 'tambourine', 44)
 
+######## RUN THE PROGRAM #########
 
-#section for adding all the events together and sorting them in the right order according to the timestamp value
-all_events = kick_events + clap_events + tambourine_events
+run_the_program = True
+print("\nHi, welcome to the irregular beat generator. This program will generate an irregular beat according to a given BPM, amount of bars and either a 5/4 or a 7/8 time signature. Lets start!\n")
 
-all_events = sorted(all_events, key=lambda x: x['timestamp'])
+while run_the_program:
+    #communication with user
 
-#copy the eventlist so i can still use the events if the user wants to output MIDI
-all_events_buffer = list(all_events)
-
-# print("after the sort:", all_events)
-print("all events here:", all_events)
-#section of the code for the audio playback
-start_time = time.time()
-
-#var for popping an event from the event_list
-event = all_events.pop(0)
-
-#while loop for playing the sequence 
-while True:
-    #var for storing current time 
-    current_time = time.time()
-    #check if the event has to be played
-    if(current_time - start_time >= event['timestamp']):
-        event['sample'].play()
-        #replace the event var with the next event in the list and check if there are events left in the event_list
-        if(all_events):
-            event = all_events.pop(0)
-        #if there are no events left in the list break the loop
-        else:
-          break
-
-    else:
-      time.sleep(0.001)    
-
-time.sleep(1)
-
-
-#section in the code for MIDI
-midi_output = 'no'
-retrieve_midi = True
-
-while retrieve_midi:
-    matches = ['yes', 'Yes', 'y', 'Y', 'YES', 'no', 'No', 'n', 'N', 'NO']
-
-    try:
-        midi_output = str(input("\nDo you want to export the rhythm you just heard to MIDI? Type yes or no.\n"))
-    except:
-        print("\nSorry, can u please type yes or no")
-    else: 
-        if midi_output in matches:
-            retrieve_midi = False
-        else: 
-         print("\nSorry, can u please type yes or no")
-
-matches_true = ['yes', 'Yes', 'y', 'Y', 'YES']
-
-if midi_output in matches_true:
-    # set the necessary values for MIDI util
-    velocity=80
-    track = 0
-    channel = 9 # corresponds to channel 10 drums
-    bpm = default_bpm
-
-    #make midiFile
-    mf = MIDIFile(1)
-
-    #ask the user the name of the midiFile 
-    retrieve_midiname = True
-    while retrieve_midiname:
+    # input the bpm 
+    retrieve_bpm = True
+    while retrieve_bpm:
         try:
-            midi_name = input("\nHow do you want to name your midi file? Please give a name between 1 and 15 characters.\n")
+            default_bpm = int(input("\nAt what BPM do you want the sequence to be played? Please give a number between 50 and 180\n"))
         except:
-            print("\nSorry can u pls give a name between 1 and 15 characters")
-        else:
-            if len(midi_name) >= 1 and len(midi_name) <= 15:
-                print("\nOkay, the name of your midiFile will be:", midi_name)
-                retrieve_midiname = False
+            print("\nSorry, can u please give a number between 50 and 180")
+        else: 
+            if default_bpm in range(50,181):
+                retrieve_bpm = False
             else:
+                print("\nSorry, can u please give a number between 50 and 180")
+
+    #input the amount of bars
+    retrieve_bars = True
+    while retrieve_bars:
+        try:
+            bars_amount = int(input("\nHow many bars do you want to be generated? Please give a number between 1 and 8\n"))
+        except:
+            print("\nSorry, can u please give a number between 1 and 8")
+        else:
+            if bars_amount in range(1,9):
+                retrieve_bars = False
+            else:
+                print("\nSorry, can u please give a number between 1 and 8")
+
+    #input the time signature 
+    retrieve_time_signature = True
+    while retrieve_time_signature:
+        try:
+            time_signature = int(input("\nWhat time signature do you want to use? Press 1 for a 5/4 time signature or press 2 for a 7/8 time signature.\n"))
+        except:
+            print("\nSorry, can u please give the number 1 or 2")
+        else: 
+            if time_signature == 1 or time_signature == 2:
+                retrieve_time_signature = False
+            else: 
+                print("\nSorry, can u please give the number 1 or 2")
+
+
+    ########## section for genarating rhythms ###########
+
+    #variable for storing the amount of eigtht notes per bar
+    amount_of_notes = time_sign_to_notes(time_signature)
+
+    #generate euclidean patterns according to the amount of notes
+    kick_pattern = euclidean_gen(amount_of_notes,random.randint(1,5),0) #starting with an offset of 0 so the rhythm always starts with a kick
+    clap_pattern = euclidean_gen(amount_of_notes,random.randint(1,5), random.randint(2,6)) #starting with an random offset between 2 and 6 so the clap will always be after the kick in the first bars
+    tambourine_pattern = euclidean_gen(amount_of_notes,random.randint(3,amount_of_notes - 3), random.randint(0,5))
+
+    #duplicated and/or variate the generated pattern according to amount of bars
+    total_rhythm_tb = rhythm_bars(tambourine_pattern, bars_amount, amount_of_notes)
+    total_rhythm_k = rhythm_bars(kick_pattern, bars_amount, amount_of_notes)
+    total_rhythm_c = rhythm_bars(clap_pattern, bars_amount, amount_of_notes)
+
+    #convert the ts of the events to durations in milliseconds and add them together afterwards
+    all_dur_tb = ts_to_dur_list(total_rhythm_tb, default_bpm)
+    all_dur_k = ts_to_dur_list(total_rhythm_k, default_bpm)
+    all_dur_c = ts_to_dur_list(total_rhythm_c, default_bpm)
+    dur_combined = all_dur_tb + all_dur_c + all_dur_c
+
+    #convert timestamps in eigtht notes to ts in actual time
+    timestamps_k = stamps8th_to_stampstime(default_bpm, total_rhythm_k)
+    timestamps_c = stamps8th_to_stampstime(default_bpm, total_rhythm_c)
+    timestamps_tb = stamps8th_to_stampstime(default_bpm, total_rhythm_tb)
+
+    #making a list with events with the funtion event_maker, giving a midi value and name 
+    kick_events = event_maker(timestamps_k, kick, all_dur_k, 'kick', 36)
+    clap_events = event_maker(timestamps_c, clap, all_dur_c, 'clap', 39)
+    tambourine_events = event_maker(timestamps_tb, tambourine, all_dur_tb, 'tambourine', 44)
+
+    #adding all the events together and sorting them in the right order according to the timestamp value
+    all_events = kick_events + clap_events + tambourine_events
+    all_events = sorted(all_events, key=lambda x: x['timestamp'])
+
+
+    ########## section for playback ###########  
+
+    #copy the eventlist so i can still use the events if the user wants to output MIDI
+    all_events_buffer = list(all_events)
+
+    # print("after the sort:", all_events)
+    print("all events here:", all_events)
+    #section of the code for the audio playback
+    start_time = time.time()
+
+    #var for popping an event from the event_list
+    event = all_events.pop(0)
+
+    #while loop for playing the sequence 
+    while True:
+        #var for storing current time 
+        current_time = time.time()
+        #check if the event has to be played
+        if(current_time - start_time >= event['timestamp']):
+            event['sample'].play()
+            #replace the event var with the next event in the list and check if there are events left in the event_list
+            if(all_events):
+                event = all_events.pop(0)
+            #if there are no events left in the list break the loop
+            else:
+                break
+
+        else:
+             time.sleep(0.001)    
+
+    time.sleep(1)
+
+
+    ########## section for MIDI output ###########  
+
+    midi_output = 'no'
+    retrieve_midi = True
+
+    while retrieve_midi:
+        matches = ['yes', 'Yes', 'y', 'Y', 'YES', 'no', 'No', 'n', 'N', 'NO']
+
+        try:
+            midi_output = str(input("\nDo you want to export the rhythm you just heard to MIDI? Type yes or no.\n"))
+        except:
+            print("\nSorry, can u please type yes or no")
+        else: 
+            if midi_output in matches:
+                retrieve_midi = False
+            else: 
+                 print("\nSorry, can u please type yes or no")
+
+    matches_true = ['yes', 'Yes', 'y', 'Y', 'YES']
+    mathces_false = ['no', 'No', 'n', 'N', 'NO']
+
+    if midi_output in matches_true:
+        # set the necessary values for MIDI util
+        velocity=80
+        track = 0
+        channel = 9 # corresponds to channel 10 drums
+        bpm = default_bpm
+
+        #make midiFile
+        mf = MIDIFile(1)
+
+        #ask the user the name of the midiFile 
+        retrieve_midiname = True
+        while retrieve_midiname:
+            try:
+                midi_name = input("\nHow do you want to name your midi file? Please give a name between 1 and 15 characters.\n")
+            except:
                 print("\nSorry can u pls give a name between 1 and 15 characters")
+            else:
+                if len(midi_name) >= 1 and len(midi_name) <= 15:
+                    print("\nOkay, the name of your midiFile will be:", midi_name)
+                    retrieve_midiname = False
+                else:
+                    print("\nSorry can u pls give a name between 1 and 15 characters")
 
-    #set name and tempo
-    time_beginning = 0
-    mf.addTrackName(track, time_beginning, midi_name)
-    mf.addTempo(track, time_beginning, bpm)
-    midi_time = 0
-    e_note = 30000/default_bpm
+        #set name and tempo
+        time_beginning = 0
+        mf.addTrackName(track, time_beginning, midi_name)
+        mf.addTempo(track, time_beginning, bpm)
+        midi_time = 0
+        e_note = 30000/default_bpm
 
-    #for loop for adding all the events to a midi file
-    for event in all_events_buffer:
-        mf.addNote(track, channel, event['midinote'], midi_time, 1.5, velocity)
-        midi_time = midi_time + (event['duration']/e_note)
+        #for loop for adding all the events to a midi file
+        for event in all_events_buffer:
+            mf.addNote(track, channel, event['midinote'], midi_time, 1.5, velocity)
+            midi_time = midi_time + (event['duration']/e_note)
+        
+        with open(midi_name+".midi",'wb') as outf:
+            mf.writeFile(outf)
+
+
+
+    retrieve_gen_rhythm = True
+
+    while retrieve_gen_rhythm:
+        matches = ['yes', 'Yes', 'y', 'Y', 'YES', 'no', 'No', 'n', 'N', 'NO']
+        try:
+            gen_rhythm = str(input("\nDo you want to generate a new rhythm? Type yes or no.\n"))
+        except:
+            print("\nSorry, can u please type yes or no")
+        else: 
+            if gen_rhythm in matches:
+                retrieve_gen_rhythm = False
+            else: 
+                 print("\nSorry, can u please type yes or no")
+
+    if gen_rhythm not in matches_true:
+        print("\n Okay, thanks for using the program. Quiting...")
+        run_the_program = False
+    else: 
+        print("\n\n Okay, lets generate a new rhythm")
+
     
-    with open(midi_name+".midi",'wb') as outf:
-        mf.writeFile(outf)
+
+
