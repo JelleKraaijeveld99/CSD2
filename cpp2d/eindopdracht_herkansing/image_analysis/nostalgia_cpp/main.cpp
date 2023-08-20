17b4e2474451e46ec5099bbb82cf38d9b171e1fb
Jelle Kraaijeveld <jelle.kraaijeveld@student.hku.nl>
2023-08-20 13:02:08 +0200

    added: class structure

--- cpp2d/eindopdracht_herkansing/image_analysis/nostalgia_cpp/main.cpp
+++ cpp2d/eindopdracht_herkansing/image_analysis/nostalgia_cpp/main.cpp
@@ -11,8 +11,10 @@ int main() {
     //initialise
     Analyse analyse;
     //function for calculating the percentage of each color in the picture
-    analyse.colorAnalyse("/Users/jellekraaijeveld/Documents/HKU1/Jaar_2/CSD/nostalgie/image_analysis/nostalgia_cpp/crayontest1.jpg");
+    analyse.colorAnalyse("/Users/jellekraaijeveld/Documents/HKU1/Jaar_2/CSD/CodingHKU/cpp2d/eindopdracht_herkansing/image_analysis/nostalgia_cpp/crayon_features_1.jpg");
     //let the program wait
+    analyse.colorPercentage();
+    analyse.sendPercentageOSC();
     waitKey(0);
     destroyAllWindows();
     return 0;
